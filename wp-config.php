3a7e6423c352484fa6c24c13e52bc708c0c393d7
Seamus D'Arcy <seamus@celsussolutions.com>
2012-04-12 15:38:52 +0100

    database config

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'db_4c43c5f8');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'user_4c43c5f8');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'Ort@Ipz91xZe)(');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'a.db.shared.orchestra.io');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
